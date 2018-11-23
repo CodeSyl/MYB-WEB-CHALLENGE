48a6f86b0bd23835c1b38eab4560ef6050e3e048
sylbtz <sylvain.lapart@gmail.com>
2018-11-23 13:40:34 +0100

    Update css link

--- src/components/TabBar/TabBar.js
+++ src/components/TabBar/TabBar.js
@@ -4,32 +4,32 @@ import './TabBar.scss';
 
 class TabBar extends React.Component {
   state = {
-    upComing: true,
+    upcoming: true,
     friends: false
   }
 
   showContent = ({ target }) => {
-
-    target.name === "upcoming" && this.setState(state => ({ ...state, upComing: true, friends: false }))
-    target.name === "friends" && this.setState(state => ({ ...state, upComing: false, friends: true }))
+    target.name === "upcoming" && this.setState(state => ({ ...state, upcoming: true, friends: false }))
+    target.name === "friends" && this.setState(state => ({ ...state, upcoming: false, friends: true }))
   }
 
   render() {
-    const { upComing, friends } = this.state;
+    const { upcoming, friends } = this.state;
     return (
       <div>
         <div className="tab__bar">
-          <div className={classNames({ "link": true, "active": upComing })}>
-            <a name="upcoming" href="#" onClick={this.showContent}>{this.props.children[0].props.title}</a>
+          <div className={classNames({ "link": true, "active": upcoming })}>
+            <a href="#" name="upcoming" onClick={this.showContent}>{this.props.children[0].props.title}</a>
           </div>
           <div className={classNames({ "link": true, "active": friends })}>
             <a href="#" name="friends" onClick={this.showContent}>{this.props.children[1].props.title}</a>
           </div>
         </div>
 
-        {upComing && this.props.children[0]}
+        {upcoming && this.props.children[0]}
         {friends && this.props.children[1]}
-      </div >
+
+      </div>
     );
 
   }
